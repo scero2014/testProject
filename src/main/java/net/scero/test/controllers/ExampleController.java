43325dc0c6a3001e609f06509005fcac6eea2f5b
Jose Luis Nogueira <jnogueira@hotelbeds.com>
2017-08-30 11:19:48 +0200

    storage test

--- src/main/java/net/scero/test/controllers/ExampleController.java
+++ src/main/java/net/scero/test/controllers/ExampleController.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.nio.file.Files;
 
 import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
