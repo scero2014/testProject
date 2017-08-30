ce4e1c73e1b7a4e9e38dc3bf4b4739bd078a017c
Jose Luis Nogueira <jnogueira@hotelbeds.com>
2017-08-30 08:33:23 +0200

    Borrado de cosas innecesarias

--- src/main/java/io/spring/guides/gs_producing_web_service/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2017.08.29 a las 03:42:14 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package io.spring.guides.gs_producing_web_service;
