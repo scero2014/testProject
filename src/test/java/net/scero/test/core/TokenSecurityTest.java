d65dfd1796b7d356dcb746f1d37f82035cead222
Jose Luis Nogueira <jnogueira@hotelbeds.com>
2017-09-07 12:11:21 +0200

    tokens y encriptaciones

--- src/test/java/net/scero/test/core/TokenSecurityTest.java
+++ src/test/java/net/scero/test/core/TokenSecurityTest.java
@@ -11,5 +11,6 @@ public class TokenSecurityTest {
         String token = ts.createJWT(new User("scero", 20, "ADMIN"), 100);
         User user = ts.authenticate(token);
         System.out.println(user.toString());
+        assertTrue("scero".equals(user.getUser()));
     }
 }
